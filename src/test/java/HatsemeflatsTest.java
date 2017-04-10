d23274138b38895b63dca3341627608863ad0285
Hendrik van der Linde <hendrik.vanderlinde@nedap.com>
2017-04-10 14:06:39 +0200

    test setup

---
+++ src/test/java/HatsemeflatsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author hendrik.vanderlinde
+ */
+public class HatsemeflatsTest {
+
+    @Test
+    public void testHats() {
+        assertThat(1, is(1));
+    }
+}
